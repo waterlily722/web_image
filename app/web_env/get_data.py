d3490544258357282f6b70384dedaf12a4484f41
waterlily722 <1603289686@qq.com>
2024-12-14 20:05:06 +0800

    update

--- app/web_env/get_data.py
+++ app/web_env/get_data.py
@@ -15,5 +15,15 @@ def get_image_data(image_id):
     cursor = conn.cursor(dictionary=True)
     cursor.execute("SELECT * FROM image_analysis WHERE id = %s", (image_id,))
     image_data = cursor.fetchone()
+    cursor.close()
     conn.close()
     return image_data
+
+def get_user_data(username):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user WHERE username = %s", (username,))
+    user_data = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return user_data
